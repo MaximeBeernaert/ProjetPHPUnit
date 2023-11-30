87786b0d43d9ff9fe11af7c02abc3f255be9f9bc
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-11-30 12:29:11 +0100

    feat(view): update mainmenu.php

--- src/Web/MainMenu.php
+++ src/Web/MainMenu.php
@@ -6,19 +6,23 @@
 ?>
 
 <div class="mainmenu">
+    <!-- Category 'IN THE NEWS' -->
     <div class="mainmenu-inthenews"> 
         <div class="mainmenu-inthenews-container">
 
-            <!-- Category 'IN THE NEWS' -->
+            <!-- TITLE -->
             <div class="mainmenu-inthenews-container-title">
                 <h2>À la une</h2>
             </div>
+            <!-- RECIPES -->
             <div class="mainmenu-inthenews-container-recipes">
+                <!-- FIRST RECIPE -->
                 <div class="mainmenu-inthenews-container-recipes-primary">
                     <div class="mainmenu-inthenews-container-recipes-primary-recipe">
                         <h3>Tarte au citron meringuée Principal</h3>
                     </div>
                 </div>
+                <!-- OTHER RECIPES -->
                 <div class="mainmenu-inthenews-container-recipes-secondary">
                     <div class="mainmenu-inthenews-container-recipes-secondary-recipe">
                         <h3>Tarte au citron meringuée Secondaire 1</h3>
@@ -30,8 +34,20 @@
             </div>
 
             
-
         </div>
     </div>
+
+    <!-- Category 'RECIPE OF THE DAY' -->
+    <div class="mainmenu-recipeoftheday">
+        <div class="mainmenu-recipeoftheday-container">
+            <!-- TITLE -->
+            <div class="mainmenu-recipeoftheday-container-title">
+                <h2>Recette du jour</h2>
+            </div>
+            <!-- RECIPE -->
+            <div class="mainmenu-recipeoftheday-container-recipe">
+                <h3>Tarte au citron meringuée</h3>
+            </div>
+        </div>
 </div>
 
