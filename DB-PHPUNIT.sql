a9347833f8aebda05c8795dcf80481ba2c9b874e
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-11-30 10:55:30 +0100

    doxs(service): update database sql

--- DB-PHPUNIT.sql
+++ DB-PHPUNIT.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `assocrecingr`
 --
 
+CREATE DATABASE IF NOT EXISTS `projetphpunit` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `assocrecingr`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
