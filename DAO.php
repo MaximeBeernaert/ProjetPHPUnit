19a9886fd6842e3d6771c2f5b0a75f78fbbd9a5c
Enzo Borges <enzo077@outlook.fr>
2023-11-30 11:53:20 +0100

    feat: CRUD for Ingredient

--- DAO.php
+++ DAO.php
@@ -97,6 +97,82 @@ class IngredientDAO
     {
         $this->bd = $bd;
     }
+
+    //Create a new ingredient
+    public function create(Ingredient $ingredient)
+    {
+        try {
+            $name = $ingredient->getName();
+            $price = $ingredient->getPrice();
+
+            $sql = "INSERT INTO ingredient (name, price) VALUES (:name, :price)";
+            $stmt = $this->bd->prepare($sql);
+
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':price', $price);
+            $stmt->execute();
+
+            return true;
+        } catch (Exception $e) {
+            echo "Error during ingredient creation: " . $e->getMessage();
+        }
+    }
+
+    //Read an ingredient
+    public function read($id)
+    {
+        try {
+            $sql = "SELECT * FROM ingredient WHERE id = :id";
+            $stmt = $this->bd->prepare($sql);
+
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $result;
+        } catch (Exception $e) {
+            echo "Error during ingredient reading: " . $e->getMessage();
+        }
+    }
+
+    //Update an ingredient
+    public function update(Ingredient $ingredient)
+    {
+        try {
+            $id = $ingredient->getId();
+            $name = $ingredient->getName();
+            $price = $ingredient->getPrice();
+
+            $sql = "UPDATE ingredient SET name = :name, price = :price WHERE id = :id";
+            $stmt = $this->bd->prepare($sql);
+
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':price', $price);
+            $stmt->execute();
+
+            return true;
+        } catch (Exception $e) {
+            echo "Error during ingredient update: " . $e->getMessage();
+        }
+    }
+
+    //Delete an ingredient
+    public function delete($id)
+    {
+        try {
+            $sql = "DELETE FROM ingredient WHERE id = :id";
+            $stmt = $this->bd->prepare($sql);
+
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+
+            return true;
+        } catch (Exception $e) {
+            echo "Error during ingredient deletion: " . $e->getMessage();
+        }
+    }
 }
 
 //CRUD for recipes table
