1426e94468727b0befb8ef53deb1b159d1372429
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-11-30 15:17:45 +0100

    feat(view): add MainMenu.css, update Header.css

--- src/Web/MainMenu.php
+++ src/Web/MainMenu.php
@@ -1,4 +1,12 @@
 <DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Style/MainMenu.css">
+</head>
+
 <?php
     require_once __DIR__ . '/../Web/Header.php';
 ?>
@@ -10,7 +18,7 @@
 
             <!-- TITLE -->
             <div class="mainmenu-inthenews-container-title">
-                <h2>À la une</h2>
+                <h2>À LA UNE</h2>
             </div>
             <!-- RECIPES -->
             <div class="mainmenu-inthenews-container-recipes">
@@ -42,10 +50,25 @@
             <div class="mainmenu-recipeoftheday-container-title">
                 <h2>Recette du jour</h2>
             </div>
-            <!-- RECIPE -->
             <div class="mainmenu-recipeoftheday-container-recipe">
-                <h3>Tarte au citron meringuée</h3>
+                <!-- RECIPE -->
+                <div class="mainmenu-recipeoftheday-container-recipe">
+                    <h3>Tarte au citron meringuée - 1</h3>
+                </div>
             </div>
+            <div class="mainmenu-recipeoftheday-container-recipe">
+                <!-- RECIPE -->
+                <div class="mainmenu-recipeoftheday-container-recipe">
+                    <h3>Tarte au citron meringuée - 2</h3>
+                </div>
+            </div>
+            <div class="mainmenu-recipeoftheday-container-recipe">
+                <!-- RECIPE -->
+                <div class="mainmenu-recipeoftheday-container-recipe">
+                    <h3>Tarte au citron meringuée - 3</h3>
+                </div>
+            </div>            
         </div>
+    </div>
 </div>
 
