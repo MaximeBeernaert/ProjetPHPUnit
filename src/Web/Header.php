f68852ac0a7185080920f914e9465b48e6678823
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-11-30 14:38:51 +0100

    feat(view): add Header.css

--- src/Web/Header.php
+++ src/Web/Header.php
@@ -1,4 +1,11 @@
-<DOCTYPE html>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Style/Header.css">
+</head>
+
 <?php
 
 
@@ -7,12 +14,11 @@
 
 <div class="header">
     <!-- TOP PART OF HEADER -->
-    <h2>Header</h2>
     <div class="header-main">
         <!-- CSS LIST OF CATEGORIES -->
         <div class ="header-main-categorylist">
             <h3>Categories</h3>
-        
+
         </div>
         <!-- LOGO (return to main menu) -->
         <div class="header-main-logo">
@@ -24,21 +30,29 @@
             <h3>Recherche</h3>
 
         </div>
+
+        <!-- LOGIN -->
+        <div class="header-main-login">
+            <h3>Connexion</h3>
+
+        </div>
     </div>
 
     <!-- BOTTOM PART OF HEADER -->
     <div class="header-secondary">
-        <div class="header-secondary-news">
-            <h3>Actus</h3>
-
-        </div>
-        <div class="header-secondary-fastrecipe">
-            <h3>Recettes très très rapides</h3>
-
-        </div>
-        <div class="header-secondary-bestrecipes">
-            <h3>Meilleures notes</h3>
-
+        <div class="header-secondary-container">
+            <div class="header-secondary-container-news">
+                <h3>Actus</h3>
+    
+            </div>
+            <div class="header-secondary-container-fastrecipe">
+                <h3>Recettes très très rapides</h3>
+    
+            </div>
+            <div class="header-secondary-container-bestrecipes">
+                <h3>Meilleures notes</h3>
+            
+            </div>
         </div>
     </div>
 </div>
