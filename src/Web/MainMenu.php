8bb6510c09854b8637a3edabd3e1e509f7038fe1
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-11-30 12:25:03 +0100

    feat(view): add mainmenu.php

---
+++ src/Web/MainMenu.php
@@ -0,0 +1,37 @@
+<DOCTYPE html>
+<?php
+
+
+
+?>
+
+<div class="mainmenu">
+    <div class="mainmenu-inthenews"> 
+        <div class="mainmenu-inthenews-container">
+
+            <!-- Category 'IN THE NEWS' -->
+            <div class="mainmenu-inthenews-container-title">
+                <h2>À la une</h2>
+            </div>
+            <div class="mainmenu-inthenews-container-recipes">
+                <div class="mainmenu-inthenews-container-recipes-primary">
+                    <div class="mainmenu-inthenews-container-recipes-primary-recipe">
+                        <h3>Tarte au citron meringuée Principal</h3>
+                    </div>
+                </div>
+                <div class="mainmenu-inthenews-container-recipes-secondary">
+                    <div class="mainmenu-inthenews-container-recipes-secondary-recipe">
+                        <h3>Tarte au citron meringuée Secondaire 1</h3>
+                    </div>
+                    <div class="mainmenu-inthenews-container-recipes-secondary-recipe">
+                        <h3>Tarte au citron meringuée Secondaire 2</h3>
+                    </div>
+                </div>
+            </div>
+
+            
+
+        </div>
+    </div>
+</div>
+
